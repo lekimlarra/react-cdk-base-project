5ad5005fad5a67f35a2fcb952f32206d9bbed87e
lekimlarra <33814049+lekimlarra@users.noreply.github.com>
2025-02-25 21:16:23 +0100

    CDK INIT

---
+++ test/react-cdk-base-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReactCdkBaseProject from '../lib/react-cdk-base-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/react-cdk-base-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReactCdkBaseProject.ReactCdkBaseProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
