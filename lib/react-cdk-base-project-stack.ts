b4b1fb6204087834a7c2e2dbfe96dda23c6b130a
lekimlarra <33814049+lekimlarra@users.noreply.github.com>
2025-02-26 14:15:42 +0100

    dot env working and bucket name

--- lib/react-cdk-base-project-stack.ts
+++ lib/react-cdk-base-project-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const bucketName = process.env.bucketName ?? "";
+console.log("bucketName: ", bucketName);
 
 export class ReactCdkBaseProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ReactCdkBaseProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ********************** WEBSITE - S3 **********************
+    const websiteBucket = new Bucket(this, "s3bucket", {
+      publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+    });
   }
 }
